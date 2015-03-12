d94f97b7ae66436a241c7d6beb6042e86d1af919
ShinigamiDay <f.pin78@gmail.com>
2015-03-12 15:18:57 +0100

    Problème dans la liste des articles, administration

--- src/ESGI/BlogBundle/Admin/ArticleAdmin.php
+++ src/ESGI/BlogBundle/Admin/ArticleAdmin.php
@@ -8,6 +8,8 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Validator\ErrorElement;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ArticleAdmin extends Admin
 {
@@ -57,6 +59,50 @@ class ArticleAdmin extends Admin
     public function prePersist($article)
     {
         $article->setUser($this->securityContext->getToken()->getUser());
+    }
+
+    public function getBatchActions()
+    {
+        // retrieve the default (currently only the delete action) actions
+        $actions = parent::getBatchActions();
+
+        // check user permissions
+        if($this->hasRoute('edit') && $this->isGranted('EDIT') && $this->hasRoute('delete') && $this->isGranted('DELETE')){
+            $actions['extend'] = array(
+                'label'            => 'Extend',
+                'ask_confirmation' => true // If true, a confirmation will be asked before performing the action
+            );
+
+            $actions['deleteNeverActivated'] = array(
+                'label'            => 'Delete never activated jobs',
+                'ask_confirmation' => true // If true, a confirmation will be asked before performing the action
+            );
+        }
+
+        return $actions;
+    }
+
+    public function batchActionDeleteNeverActivatedIsRelevant()
+    {
+        return true;
+    }
+
+    public function batchActionDeleteNeverActivated()
+    {
+        if ($this->admin->isGranted('EDIT') === false || $this->admin->isGranted('DELETE') === false) {
+            throw new AccessDeniedException();
+        }
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $nb = $em->getRepository('EnsJobeetBundle:Job')->cleanup(60);
+
+        if ($nb) {
+            $this->get('session')->setFlash('sonata_flash_success',  sprintf('%d never activated jobs have been deleted successfully.', $nb));
+        } else {
+            $this->get('session')->setFlash('sonata_flash_info',  'No job to delete.');
+        }
+
+        return new RedirectResponse($this->admin->generateUrl('list',$this->admin->getFilterParameters()));
     }
 
 }
