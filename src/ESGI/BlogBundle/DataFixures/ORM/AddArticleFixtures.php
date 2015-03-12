ba1798501eb6231234f5bca5cf5f39ccfb84b784
nikosFram <nicolas.framery.2504@gmail.com>
2015-03-12 10:05:48 +0100

    Modif fixtures

--- src/ESGI/BlogBundle/DataFixures/ORM/AddArticleFixtures.php
+++ src/ESGI/BlogBundle/DataFixures/ORM/AddArticleFixtures.php
@@ -3,17 +3,37 @@
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use ESGI\BlogBundle\Entity\Article;
+use ESGI\UserBundle\Entity\User;
 
 class AddArticleFixtures implements FixtureInterface
 {
     function load(ObjectManager $manager)
     {
         $i = 1;
+        $id = 10;
+
+        #region essaiGetUser
+        //$repository = $this->getDoctrine()
+//                ->getRepository('AcmeStoreBundle:Product');
+//            $user = $repository->find($id);
+//            $this->container->get('security.context')->getToken()->getUser();
+        #endregion
+
+        /* Création d'un user obligatoire qui va être lié aux articles */
+        $user = new User();
+        $user->setUsername('User name n°');
+        $user->setUsernameCanonical('nikosFram');
+        $user->setEmail('nicolas.framery.@gmail.com');
+        $user->setEmailCanonical('nicolas.framery.@gmail.com');
+        $user->setEnabled(true);
+        $user->setPassword('nicolas');
+        $user->setLocked(false);
+        $manager->persist($user);
+        $manager->flush();
 
         while ($i <= 10) {
-
             $article = new Article();
-            $article->setAuthor('nico N°' . $i);
+            $article->setUser($user);
             $article->setBody("Article n°" . $i);
             $article->setCategory(null);
             $article->setIsPublished(true);
