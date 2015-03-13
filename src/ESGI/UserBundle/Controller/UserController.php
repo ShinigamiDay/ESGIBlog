3126fb673957b7f6a23b3b1de2adf700b801d04a
GuillaumeGth <guillaume.zarb@gmail.com>
2015-03-13 12:54:10 +0100

    registration

--- src/ESGI/UserBundle/Controller/UserController.php
+++ src/ESGI/UserBundle/Controller/UserController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use ESGI\UserBundle\Entity\User;
 //use ESGI\UserBundle\Form\Type\ProfileEditFormType;
 use ESGI\UserBundle\Form\Type\RegistrationFormType;
+use ESGI\UserBundle\Form\Type\ProfileFormType;
 //use ESGI\UserBundle\Form\Type\UserEditFormType;
 //use ESGI\UserBundle\Form\Type\ChangePasswordFormType;
 
@@ -23,6 +24,29 @@ class UserController extends Controller
         $userManager = $this->container->get('fos_user.user_manager');
         $user = $userManager->createUser();
         $form = $this->createForm(new RegistrationFormType(), $user);
+
+        $request = $this->get('request');
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                // On active directement l'utilisateur sans qu'il n'ait besoin d'activer son compte
+                $user->setEnabled(true);
+                $userManager->updateUser($user);
+            }
+        }
+
+        return array(
+                // ...
+            );
+    }
+
+     public function profileAction()
+    {
+
+        $userManager = $this->container->get('fos_user.user_manager');
+        $user = $userManager->createUser();
+        $form = $this->createForm(new ProfileFormType(), $user);
 
         $request = $this->get('request');
 
