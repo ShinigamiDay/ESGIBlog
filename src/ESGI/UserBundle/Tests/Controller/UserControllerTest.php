10ca545d0ec43fc55a7248d10b7158d96662da04
nikosFram <nicolas.framery.2504@gmail.com>
2015-03-13 12:34:21 +0100

    Last commit nico

--- src/ESGI/UserBundle/Tests/Controller/UserControllerTest.php
+++ src/ESGI/UserBundle/Tests/Controller/UserControllerTest.php
@@ -13,4 +13,23 @@ class UserControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/registration');
     }
 
+    public function subscribe()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/register');
+
+        $form = $crawler->selectButton('Register')->form();
+
+        // définit certaines valeurs
+        $form['fos_user_registration_form_username'] = 'cybard';
+        $form['fos_user_registration_form_email'] = 'nicolas.cybard4@gmail.com';
+        $form['fos_user_registration_form_plainPassword_first'] = 'nicolas';
+        $form['fos_user_registration_form_plainPassword_second'] = 'nicolas';
+
+        // soumet le formulaire
+        $crawler = $client->submit($form);
+
+        $this->assertEquals(1, $crawler->filter('html:contains("The user has been created successfully")')->count());
+    }
+
 }
